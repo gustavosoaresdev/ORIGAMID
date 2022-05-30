268f221d98e927c2f91981b741b54e1b1363fac0
guuhgst <gustavoprogramador18@gmail.com>
2022-05-30 15:12:11 -0300

    Inicio de Exercício 04

---
+++ Exercícios/Exercicio_04.js
@@ -0,0 +1,30 @@
+//  Exercício de JS #4
+
+
+
+//  Crie uma função para verificar se um valor é truthy
+
+//  Crie uma função matemática que retorne o perimetro de um quadrado
+//  Lembrando: perimetro é a soma dos quatro lados do quadrado
+
+//  Crie uma função que retorne o seu nome completo
+//  Ela deve possuir os parâmetros: nome e sobrenome
+
+//  Crie uma função que verifica se um número é par
+
+//  Crie uma função que retorna o tipo de dado do argumento passado nela (typeof)
+
+//  addEventListener é uma função nativa do JavaScript
+//  o primeiro parâmetro é o evento que ocorre e o segundo o Callback
+//  utilize essa função para mostrar no console o seu nome completo
+//  quando o evento "Click" ocorrer
+
+//  Corrija o erro abaixo
+function jaVisitei(paisesVisitados) {
+    var totalPaises = 193;
+    return `Já visitei ${paisesVisitados} do total de ${totalPaises} paises`;
+}
+
+function precisoVisitar(visitar) {
+    return `Ainda faltam ${totalPaises - visitar} paises para visitar`;
+}
