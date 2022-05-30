9a83e9655f4ad05ce76f66b0e39d23ef1c4f24fc
guuhgst <gustavoprogramador18@gmail.com>
2022-05-30 00:15:45 -0300

    Inicio de Exercício 03

---
+++ Exercícios/Exercicio_03.js
@@ -0,0 +1,37 @@
+//  Exercício de JS #3
+
+
+
+//  Verifique se a sua idade é maior do que a de algum parente
+//  Dependendo do resultado coloque no console 'É maior', 'É igual'
+
+//  Qual valor é retornado na seguinte expressão?
+var expressao = 5 - 2 && 5 - " " && 5 - 2;
+if (expressao) {
+  console.log("trueeeeeee");
+}
+
+//  Verifique se as seguintes variáveis são truthy ou falsy
+var nome = "gustavo";
+var idade = 19;
+var possuiDoutorado = false;
+var empregoFuturo;
+var dinheiroNaConta = 0;
+
+//  Compare o total de habitantes do Brasil com a China (valor em milhões)
+var brasil = 207;
+var china = 1340;
+
+//  Oque irá aparecer no console?
+if ("Gato" === "gato" && 5 > 2) {
+  console.log("Verdadeiro");
+} else {
+  console.log("Falso");
+}
+
+//  Oque irá aparecer no console?
+if ("Gato" === "gato" || 5 > 2) {
+  console.log("Gato" && "Cão");
+} else {
+  console.log("Falso");
+}
