ef03655d758141393fbc37f46c8da587d9e4d6d6
guuhgst <gustavoprogramador18@gmail.com>
2022-05-29 23:57:27 -0300

    Inicio de Exercício 02

---
+++ Exercícios/Exercicio_02.js
@@ -0,0 +1,15 @@
+//  Exercício de JS #2
+
+
+
+//  Qual o resultado da seguinte expressão?
+
+//  O resultado é: 35
+
+//  Crie duas expressões que retorem NaN
+
+//  Somar a string '200' com o número 50 e retornar 250
+
+//  Incremente o número 5 e retorne o seu valor incrementado
+
+//  Como dividir o peso por 2?
