1eaac54658a3501da798f2075b2da271a0812ebe
guuhgst <gustavoprogramador18@gmail.com>
2022-05-29 23:41:12 -0300

    Inicio de Exercícios

---
+++ Exercícios/Exercício_01.js
@@ -0,0 +1,16 @@
+//  Exercício de JS #1
+
+
+
+//  Declare uma variável contendo uma string
+
+//  Declare uma variável contendo um número dentro de uma string
+
+//  Declare uma variável com a sua idade;
+
+//  Declare duas variáveis, uma com seu nome
+//  e outra com seu sobrenome e some as duas
+
+//  Coloque a seguinte frase em uma variável: It's time 
+
+//  Verifique o tipo da variável que contém o seu nome
