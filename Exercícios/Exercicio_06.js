bf13dd3e37c7e6dabaccd2672fe25ba005d63560
guuhgst <gustavoprogramador18@gmail.com>
2022-05-31 11:36:06 -0300

    Inicio de Exercício 06

---
+++ Exercícios/Exercicio_06.js
@@ -0,0 +1,10 @@
+//  Exercício de JS #6
+
+
+
+//  Nomeie 3 propriedades ou métodos de string
+
+//  Nomeie 5 propriedades ou métodos de elementos do DOM
+
+//  Busque na web um objeto (método) capaz de interagir com o clipboard,
+//  clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
