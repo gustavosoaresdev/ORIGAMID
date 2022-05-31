de7419a6587ffb4dd2b6cb28cb52c16225aa25de
guuhgst <gustavoprogramador18@gmail.com>
2022-05-31 11:43:27 -0300

    segundo exercício feito

--- Exercícios/Exercicio_06.js
+++ Exercícios/Exercicio_06.js
@@ -11,6 +11,18 @@ console.log(verifyNome.toUpperCase());          //  2
 console.log(verifyNome.length);                 //  3
 
 //  Nomeie 5 propriedades ou métodos de elementos do DOM
+var btnA = document.querySelector('.btnA');
+
+btnA.innerText;                         //  Texto do elemento - Click                       //  1
+btnA.classList.add('color-blue');       //  Adicionando uma classe para o elemento          //  2
+btnA.classList.remove('color-blue');    //  Removendo a classe (color-blue) do elemento     //  3
+
+document.body.appendChild(btnA);        //  Colocando o elemento dentro do body             //  4
+
+//  Adicionando evento de click para o elemento e retornado Clicked no console              //  5
+btnA.addEventListener('click', function() {
+    console.log('Clicked!');
+});
 
 //  Busque na web um objeto (método) capaz de interagir com o clipboard,
 //  clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
