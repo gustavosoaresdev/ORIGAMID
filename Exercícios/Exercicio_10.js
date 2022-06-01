3e6d885e36fcea9fde3593934fe94485d0fa728d
guuhgst <gustavoprogramador18@gmail.com>
2022-06-01 13:40:59 -0300

    Inicio de Exercício 10

---
+++ Exercícios/Exercicio_10.js
@@ -0,0 +1,11 @@
+//  Exercício de JS #10
+
+
+
+//  Retorne o url da página atual utilizando o objeto window
+
+//  Selecione o primeiro elemento da página que possua a classe ativo
+
+//  Retorne a linguagem do navegador
+
+//  Retorne a largura da janela
