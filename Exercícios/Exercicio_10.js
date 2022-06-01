fb00beb2b35fb3ef426174c215b59b555220575a
guuhgst <gustavoprogramador18@gmail.com>
2022-06-01 13:42:43 -0300

    primeiro exercício feito

--- Exercícios/Exercicio_10.js
+++ Exercícios/Exercicio_10.js
@@ -3,6 +3,8 @@
 
 
 //  Retorne o url da página atual utilizando o objeto window
+const url = location.href;
+console.log('url da página:', url);
 
 //  Selecione o primeiro elemento da página que possua a classe ativo
 
