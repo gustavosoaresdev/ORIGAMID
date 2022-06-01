93c16f699a51a5474d3463f26c160a9cee667a00
guuhgst <gustavoprogramador18@gmail.com>
2022-06-01 14:00:56 -0300

    quarto exercício feito

--- Exercícios/Exercicio_11.js
+++ Exercícios/Exercicio_11.js
@@ -15,5 +15,11 @@ const linksInternos = document.querySelectorAll('[href^="#"]');
 console.log(linksInternos);
 
 //  Selecione o primeiro h2 dentro de .animais-descricao
+const firstH2 = document.querySelector('.animais-descricao h2');
+console.log(firstH2);
+//  OU
+const firsttH2 = document.querySelector('.animais-descricao');
+const h2 = firsttH2.querySelector('h2');
+console.log(h2);
 
 //  Selecione o último p do site
