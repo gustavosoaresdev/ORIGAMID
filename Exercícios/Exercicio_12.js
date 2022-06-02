9b589b7e4aeb4d3e63a2d2a24400d78c6a37d325
guuhgst <gustavoprogramador18@gmail.com>
2022-06-02 14:33:07 -0300

    terceiro exercício feito

--- Exercícios/Exercicio_12.js
+++ Exercícios/Exercicio_12.js
@@ -11,13 +11,14 @@ paragrafos.forEach(item => console.log(item.innerText));
 
 //  Como corrigir os erros abaixo?
 const imgs = document.querySelectorAll('img');
-imgs.forEach(item, index => {
+imgs.forEach((item, index) => {
     console.log(item, index)
 });
 
 let i = 0;
-imgs.forEach( => {
+imgs.forEach(() => {
     console.log(i++);
 });
 
 imgs.forEach(() => i++);
+console.log(i);
