f22c74e2a86a68fd496e9934c37fbe1884f49e37
guuhgst <gustavoprogramador18@gmail.com>
2022-06-03 15:05:04 -0300

    terceiro exercício feito

--- Exercícios/Exercicio_13.js
+++ Exercícios/Exercicio_13.js
@@ -12,5 +12,10 @@ itemsDoMenu.forEach(item => item.classList.remove('ativo'));
 itemsDoMenu[0].classList.add('ativo');
 
 //  Verifique se as imagens possuem o atributo alt
+const imgs = document.querySelectorAll('img');
+imgs.forEach(item => {
+    const possuiAlt = item.hasAttribute('alt');
+    console.log(possuiAlt);
+});
 
 //  Modifique o href do link externo do menu
