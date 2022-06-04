0c7e300e6a7c9c523bf30a90e47b1c2c77590b86
guuhgst <gustavoprogramador18@gmail.com>
2022-06-04 18:05:16 -0300

    terceiro exercício feito

--- Exercícios/Exercicio_14.js
+++ Exercícios/Exercicio_14.js
@@ -27,6 +27,16 @@ window.onload = function() {
 //  Verifique se os links da página possuem
 //  o mínimo recomendado para telas utilizadas
 //  com o dedo. (48px/48px de acordo com o google)
+const links = document.querySelectorAll('a');
+links.forEach(item => {
+    const px48 = item.getBoundingClientRect();
+
+    if(px48.width >= 48 && px48.height >= 48) {
+        console.log(item, 'Possui boa acessibilidade');
+    }   else {
+            console.log(item, 'Não possui boa acessibilidade');
+        }
+});
 
 //  Se o browser for menor que 720px,
 //  adicione a classe menu-mobile ao menu
