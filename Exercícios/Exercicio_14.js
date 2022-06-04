72d17d0f9bb4233a481801bf028ecba058df23a9
guuhgst <gustavoprogramador18@gmail.com>
2022-06-04 18:03:01 -0300

    segundo exercício feito

--- Exercícios/Exercicio_14.js
+++ Exercícios/Exercicio_14.js
@@ -9,6 +9,20 @@ const imageTop = firstImage.offsetTop;
 console.log(imageTop);
 
 //  Retorne a soma da largura de todas as imagens
+function sumImages() {
+    const allImages = document.querySelectorAll('img');
+    let sum = 0;
+
+    allImages.forEach(item => {
+        sum += item.offsetWidth;
+    });
+
+    console.log(sum);
+}
+
+window.onload = function() {
+    sumImages();
+}
 
 //  Verifique se os links da página possuem
 //  o mínimo recomendado para telas utilizadas
