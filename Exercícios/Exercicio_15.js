cd8f16cc504658fceaad50e8bc137c7b08b8299e
guuhgst <gustavoprogramador18@gmail.com>
2022-06-05 17:51:00 -0300

    Inicio de Exercício 15

---
+++ Exercícios/Exercicio_15.js
@@ -0,0 +1,17 @@
+//  Exercício de JS #15
+
+
+
+//  Quando o usuário clicar nos links internos do site,
+//  adicione a clase ativo ao item clicado e remova dos
+//  demais itens caso eles possuam a mesma. Previna
+//  o comportamento padrão desses links
+
+
+//  Selecione todos os elementos do site começando a partir do body,
+//  ao clique mostre exatamente quais elementos estão sendo clicados
+
+//  Utilizando o código anterior, ao invés de mostrar no console,
+//  remova o elemento que está sendo clicado, o método remove() remove um elemento
+
+//  Se o usuário clicar na tecla (t), aumente todo o texto do site
