e9a46cc450ee884843961ef43dc7ad04a15e8fa2
guuhgst <gustavoprogramador18@gmail.com>
2022-06-08 13:04:23 -0300

    Inicio de Exercício 16

---
+++ Exercícios/Exercicio_16.js
@@ -0,0 +1,11 @@
+//  Exercício de JS #16
+
+
+
+//  Duplique o menu e adicione ele em copy
+
+//  Selecione o primeiro DT da DL de faq
+
+//  Selecione o DD referente ao primeiro DT
+
+//  Substitua o conteúdo HTML de .faq pelo de .animais
