23372723692cecb0248d213e0d62c3e34708a6c5
guuhgst <gustavoprogramador18@gmail.com>
2022-06-12 05:16:15 -0300

    Inicio de Exercício 18

---
+++ Exercícios/Exercicio_18.js
@@ -0,0 +1,26 @@
+//  Exercício de JS #18
+
+
+
+//  Crie uma função construtora de Pessoas
+//  Deve conter nome, sobrenome e idade
+//  Crie um método no protótipo que retorne
+//  o nome completo da pessoa
+
+//  Liste os métodos acessados por
+//  dados criados com NodeList,
+//  HTMLCollection, Document
+
+//  Liste os construtores dos dados abaixo
+const li = document.querySelector('li');
+
+li;
+li.click;
+li.innerText;
+li.value;
+li.hidden;
+li.offsetLeft;
+li.click();
+
+//  Qual o construtor do dado abaixo?
+li.hidden.constructor.name;
