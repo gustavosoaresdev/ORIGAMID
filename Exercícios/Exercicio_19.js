09577de16dd05ec73e73f2ca08ff5854044b11b5
guuhgst <gustavoprogramador18@gmail.com>
2022-06-15 22:38:50 -0300

    segundo exercício feito

--- Exercícios/Exercicio_19.js
+++ Exercícios/Exercicio_19.js
@@ -10,6 +10,11 @@ Function
 Array
 
 //  Liste 5 objetos do browser
+window
+alert
+NodeList
+HTMLCollection
+Element
 
 //  Liste 2 Métodos, Propriedades ou Objetos
 //  presentes no Chrome que não existem no Firefox
