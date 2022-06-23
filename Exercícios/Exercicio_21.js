abc135ea7cc3028a1cdf1a4f1b9ed794cb380c2d
guuhgst <gustavoprogramador18@gmail.com>
2022-06-23 16:19:07 -0300

    terceiro exercício feito

--- Exercícios/Exercicio_21.js
+++ Exercícios/Exercicio_21.js
@@ -16,3 +16,17 @@ console.log(maiorNumero);
 //  os números com centavos arredondados
 //  depois retorne a soma total
 const listaPrecos = ['R$ 59,99', ' R$ 100,222', 'R$ 230  ', 'r$  200'];
+
+function limparPrecos(preco) {
+    preco = +preco.toUpperCase().replace('R$', '').trim().replace(',', '.');
+    preco = +preco.toFixed(2);
+    return preco;
+}
+
+let somarPrecos = 0;
+listaPrecos.forEach(preco => {
+    somarPrecos += limparPrecos(preco);
+});
+
+console.log(somarPrecos.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}));
+limparPrecos(listaPrecos[1]);
